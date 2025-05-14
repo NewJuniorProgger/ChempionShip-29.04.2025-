c77ef588f4ac07b5e9e568a529d2c20c24682ac6
NewJuniorProgger <bojlkobckiu@gmail.com>
2025-05-14 21:29:14 +0200

    12

--- lib/core/brand/Brand.dart
+++ lib/core/brand/Brand.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class BrandColors {
-  static Color get red => Color.fromRGBO(241, 75, 60, 0.801);
+   static Color get red => Color.fromRGBO(248, 114, 101, 1);
 
-  static Color get accent => Color.fromRGBO(198, 216, 255, 1);
+  static Color get accent => Color.fromRGBO(72, 178, 231, 1);
 
-  static Color get disable => Color.fromRGBO(60, 120, 124, 1);
+  static Color get disable => Color.fromRGBO(43, 107, 139, 1);
 
   static Color get subTextLight => Color.fromRGBO(216, 216, 216, 1);
 
-  static Color get background => Color.fromRGBO(194, 194, 255, 1);
+  static Color get background => Color.fromRGBO(247, 247, 249, 1);
 
   static Color get block => Color.fromRGBO(255, 255, 255, 1);
 
-  static Color get text => Color(0x7F424242);
+  static Color get text => Color.fromRGBO(43, 43, 43, 1);
 
   static Color get subTextDark => Color.fromRGBO(112, 123, 129, 1);
 }
--- lib/layers/domain/presentation/screens/home_screen.dart
+++ lib/layers/domain/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_router/flutter_router.dart';
 import 'package:matule/core/brand/Brand.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,14 +12,29 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Center(),
-        width: 335,
-        height: 50,
-        decoration: BoxDecoration(
-          color: BrandColors.accent,
-          borderRadius: BorderRadius.circular(45),
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(left: 25, right: 25, top: 572),
+        height: 36,
+        width: 456,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(45),
+            color: BrandColors.accent,
+              child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('ДАЛЕЕ'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: BrandColors.accent,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ],
+            
+          ),
+          ),
         ),
       ),
     );
