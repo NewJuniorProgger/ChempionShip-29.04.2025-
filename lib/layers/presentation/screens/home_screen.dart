72068158f69a5616e93cbeb20f84eb8f595c9cd3
NewJuniorProgger <bojlkobckiu@gmail.com>
2025-05-27 11:03:11 +0200

    12

--- lib/layers/presentation/screens/home_screen.dart
+++ lib/layers/presentation/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:matule/core/brand/Brand.dart';
 
@@ -11,14 +12,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Center(),
-        width: 335,
-        height: 50,
-        decoration: BoxDecoration(
-          color: BrandColors.accent,
-          borderRadius: BorderRadius.circular(45),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 29, vertical: 38),
+        width: double.maxFinite,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(45)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "Привет!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28),
+            ),
+            Text(
+              "Заполните свои данные или продолжите \n через социальные медиа",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28 / 2),
+            ),
+
+            SizedBox(height: 52),
+            Text("email"),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "",
+                border: InputBorder.none,
+                hintText: "abc@gmail.com",
+              ),
+            ),
+
+            SizedBox(height: 50),
+            Text("password"),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "",
+                border: InputBorder.none,
+                hintText: "Pass_2444",
+              ),
+            ),
+
+            SizedBox(height: 65),
+            CupertinoButton(
+              color: BrandColors.accent,
+              onPressed: () {},
+              child: Text(
+                "Войти",
+                style: TextStyle(color: BrandColors.subTextLight),
+              ),
+            ),
+          ],
         ),
       ),
     );
