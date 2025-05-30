91c80b0f517d26e849e2db8cbbd70e47c7201ee2
NewJuniorProgger <bojlkobckiu@gmail.com>
2025-05-30 19:51:05 +0200

    Two Years later......

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -2,7 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:matule/layers/presentation/screens/home_screen.dart';
 import 'package:matule/layers/presentation/screens/notification_screen.dart';
 import 'package:matule/layers/presentation/screens/root_screen.dart';
-
+import 'package:matule/layers/presentation/screens/OutDoor.dart';
 class RouterConfigGo {
   static GoRouter get router => GoRouter(
     routes: [
@@ -12,11 +12,11 @@ class RouterConfigGo {
                 RootScreen(statefulNavigationShell: navigationShell),
         branches: [
           StatefulShellBranch(
-            routes: [
+             routes: [
+              GoRoute(path: '/home', builder: (context, state) => HomeScreen()),
               GoRoute(
-                path: '/',
-                builder: (context, state) => HomeScreen(),
-                name: 'home',
+                path: '/outdoor',
+                builder: (context, state) => Outdoor(),
               ),
             ],
           ),
--- lib/layers/presentation/screens/notification_screen.dart
+++ lib/layers/presentation/screens/notification_screen.dart
@@ -1,85 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:matule/core/brand/Brand.dart';
+import 'package:go_router/go_router.dart';
 
-class NotificationScreen extends StatelessWidget {
+class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
 
   @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 29, vertical: 38),
-        width: double.maxFinite,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(45)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Привет!",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900),
-            ),
-            Text(
-              "Заполните свои данные или продолжите \n через социальные медиа",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 28 / 2),
-            ),
+    return Container(
+      color: BrandColors.background,
 
-            SizedBox(height: 52),
-            Text(
-              "email",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelStyle: TextStyle(fontSize: 15),
-                labelText: "abc@gmail.com",
-                border: InputBorder.none,
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-              ),
-            ),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
 
-            SizedBox(height: 50),
-            Text(
-              "password",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
-            ),
-            TextField(
-              obscureText: invise,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelStyle: TextStyle(fontSize: 15),
-                labelText: "pass_2444",
-                border: InputBorder.none,
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      invise = !invise;
-                    });
-                  },
-                  icon: Icon(
-                    invise
-                        ? Icons.remove_red_eye_outlined
-                        : Icons.remove_red_eye,
-                  ),
-                ),
-              ),
-            ),
+      child: Column(
+        children: [
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 1,
 
-            SizedBox(height: 65),
-            CupertinoButton(
-              color: BrandColors.accent,
-              onPressed: () {
-                context.go("/notification");
-              },
-              child: Text(
-                "Войти",
-                style: TextStyle(color: BrandColors.subTextLight),
-              ),
+              childAspectRatio: 2,
+
+              crossAxisSpacing: 10,
+
+              mainAxisSpacing: 10,
+
+              padding: EdgeInsets.all(8),
+
+              children: List.generate(16, (index) => NotificationScreen()),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
